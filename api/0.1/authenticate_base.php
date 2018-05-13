58bc70ddc8fb42d951ff14e120af4ca7467a0831
FoxInFlame <burningfoxinflame@gmail.com>
2018-05-13 21:00:33 +0900

    Fix redirection

--- api/0.1/authenticate_base.php
+++ api/0.1/authenticate_base.php
@@ -11,8 +11,8 @@ A Part of the unofficial ManageBacAPI.
 */
 
 function getSession($username, $password, $domain = "intsch") {
+
   $authenticated = false; // Initial Value
-  
   
   // ----------------------------------------------
   // First cURL to get cookies and the csrf token.
@@ -60,7 +60,7 @@ function getSession($username, $password, $domain = "intsch") {
   
   
   // ----------------------------------------------
-  // Second cURL to login to /sessions. The response is a 302 redirect to a horribly long URL with a payload parameter, or a 200 OK on failure.
+  // Second cURL to login to /sessions. The response is a 302 redirect to /student, or a 200 OK on failure.
   // ----------------------------------------------
   
   // Request to login using the csrf_token gained above and the cookies.
@@ -91,74 +91,18 @@ function getSession($username, $password, $domain = "intsch") {
   preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
   preg_match_all('/^Location:(.*)$/mi', $response, $matches2);
   $cookies = array();
-  $nextCurlcookies = "";
-  foreach($matches[1] as $item) {
-    parse_str($item, $cookie);
-    $cookies = array_merge($cookies, $cookie);
-    if(array_key_exists("__cfduid", $cookie)) {
-      $nextCurlcookies .= "__cfduid=" . $cookie["__cfduid"] . ";";
-    } else if(array_key_exists("_managebac_session", $cookie)) {
-      $nextCurlcookies .= "_managebac_session=" . $cookie["_managebac_session"] . "; request_method=POST;";
-    }
-  }
-  curl_close($ch);
-  $payloadURL = trim($matches2[1][0]);
-  
-  // ----------------------------------------------
-  // Third cURL to the long URL obtained in the second URL. The response is yet another 302 redirect to /launchpad/api/handle_forward_notification on success, or to /login on failure.
-  // ----------------------------------------------
-  
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $payloadURL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HEADER, true);
-  curl_setopt($ch, CURLOPT_COOKIE, $nextCurlcookies);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  $response = curl_exec($ch);
-  list($header, $body) = explode("\r\n\r\n", $response, 2);
-  preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
-  preg_match_all('/^Location:(.*)$/mi', $response, $matches2);
-  $cookies = array();
-  $requestCurlcookies = "";
-  foreach($matches[1] as $item) {
-    parse_str($item, $cookie);
-    $cookies = array_merge($cookies, $cookie);
-    if(array_key_exists("__cfduid", $cookie)) {
-      $nextCurlcookies .= "__cfduid=" . $cookie["__cfduid"] . ";";
-    } else if(array_key_exists("_managebac_session", $cookie)) {
-      $nextCurlcookies .= "_managebac_session=" . $cookie["_managebac_session"] . "; request_method=POST;";
-    }
-  }
-  curl_close($ch);
-  $handleURL = trim($matches2[1][0]);
-  
-  // ----------------------------------------------
-  // Fourth and final cURL to handle_forward_notification obtained in the third URL. The response is a 302 redirect to /student on success, or to /login on failure.
-  // ----------------------------------------------
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $handleURL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HEADER, true);
-  curl_setopt($ch, CURLOPT_COOKIE, $nextCurlcookies);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  $response = curl_exec($ch);
-  list($header, $body) = explode("\r\n\r\n", $response, 2);
-  preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
-  preg_match_all('/^Location:(.*)$/mi', $response, $matches2);
-  $cookies = array();
   $requestCurlcookies = "";
   foreach($matches[1] as $item) {
     parse_str($item, $cookie);
     $cookies = array_merge($cookies, $cookie);
     if(array_key_exists("__cfduid", $cookie)) {
       $requestCurlcookies .= "__cfduid=" . $cookie["__cfduid"] . ";";
-    } else if(array_key_exists("user_id", $cookie)) {
-      $requestCurlcookies .= "user_id=" . $cookie["user_id"] . ";";
-    }
-    if(array_key_exists("_managebac_session", $cookie)) {
+    } else if(array_key_exists("_managebac_session", $cookie)) {
       $requestCurlcookies .= "_managebac_session=" . $cookie["_managebac_session"] . "; request_method=POST;";
     }
   }
+  curl_close($ch);
+  $payloadURL = trim($matches2[1][0]);
   
   if(strpos(trim($matches2[1][0]), "/student") !== false) {
     $authenticated = true;
@@ -176,5 +120,6 @@ function getSession($username, $password, $domain = "intsch") {
     http_response_code(401);
     die();
   }
+  
 }
 ?>
